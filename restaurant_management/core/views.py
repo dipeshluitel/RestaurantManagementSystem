c4031ad9ccf11aaa449d5d24d5ca00d4b7383a36
Dipesh <dipeshluitel40@gmail.com>
2025-11-23 10:22:30 +0545

    adding new item column in kitchen dashboard

--- restaurant_management/core/views.py
+++ restaurant_management/core/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from .forms import OrderForm, PlaceOrderForm
 
-from .models import MenuItems, Order
+from .models import MenuItems, Order, OrderItem
 
 # Create your views here.
 class CustomLogin(LoginView):
@@ -57,7 +57,7 @@ def kitchen_dashboard(request):
     if request.user.profile.role != 'Kitchen':
       return redirect('login')
     time=datetime.datetime.now()
-    orders =  Order.objects.filter(status__in = ["pending","cooking"])
+    orders =  Order.objects.filter(status__in = ["pending","cooking"]).prefetch_related('items')
             
     return render(request, 'kitchen_dashboard.html',{'orders':orders,'time':time})
 
